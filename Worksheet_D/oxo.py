50501c553d53f4c4d620da77375a4d2ee7b4ffba
CallumPank <callpank97@gmail.com>
2016-11-07 11:38:07 +0000

    Made Changes

--- Worksheet_D/oxo.py
+++ Worksheet_D/oxo.py
@@ -4,60 +4,65 @@ from __builtin__ import xrange
 class OxoBoard:
     def __init__(self):
         """ The initialiser. Initialise any fields you need here. """
-        self.oxoboard = {(0, 0): 0, (1, 0): 0, (2, 0): 0, (0, 1): 0, (1, 1): 0, (2, 1): 0, (0, 2): 0, (1, 2): 0,
-                         (2, 2): 0}
+        self.oxoboard = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-        def get_square(self, x, y):
-            """ Return 0, 1 or 2 depending on the contents of the specified square. """
-        self.x = x
-        self.y = y
-        return self.oxoboard[x,y]
+    def get_square(self, x, y):
+        """ Return 0, 1 or 2 depending on the contents of the specified square. """
+        Tile = x + y * 3
+        return self.oxoboard[Tile]
 
 
     def set_square(self, x, y, mark):
         """ If the specified square is currently empty (0), fill it with mark and return True.
             If the square is not empty, leave it as-is and return False. """
-        if self.oxoboard[x,y] == 0:
-            self.oxoboard[x,y] = mark
+        Tile = x + y * 3
+        if self.oxoboard[Tile] == 0:
+            self.oxoboard[Tile] = mark
             return True
         else:
             return False
 
-
-        raise NotImplementedError("TODO: implement set_square")
-
     def is_board_full(self):
         """ If there are still empty squares on the board, return False.
             If there are no empty squares, return True. """
-        Board = 9
-        for i in xrange(0, 3):
-            for j in yrange(0, 3):
-                if self.board[i, j] > 0:
-                    Board -= 1
-                    if Board == 0:
-                        return True
-                    return False
-
-        raise NotImplementedError("TODO: implement is_board_full")
+        for i in xrange (9):
+            if self.oxoboard[i] == 0:
+                return False
+        return True
 
     def get_winner(self):
         """ If a player has three in a row, return 1 or 2 depending on which player.
             Otherwise, return 0. """
-        if self.oxoboard[1, 1] != 0:
-            if self.oxoboard[1, 1] == self.oxoboard[0, 0]:
-                if self.oxoboard[2 ,2] == self.oxoboard[1, 1]:
-                    return current_player
-                if self.oxoboard[2, 0] == self.oxoboard[1, 1]:
-                    if self.oxoboard[0, 2] == self.oxoboard[1, 1]:
-                        return current_player
-
-        raise NotImplementedError("TODO: implement get_winner")
+        if self.oxoboard[0] == self.oxoboard[1] == self.oxoboard[2] != 0:
+            return self.oxoboard[0]
+        if self.oxoboard[3] == self.oxoboard[4] == self.oxoboard[5] != 0:
+            return self.oxoboard[3]
+        if self.oxoboard[6] == self.oxoboard[7] == self.oxoboard[8] != 0:
+            return self.oxoboard[6]
+        if self.oxoboard[0] == self.oxoboard[3] == self.oxoboard[6] != 0:
+            return self.oxoboard[0]
+        if self.oxoboard[1] == self.oxoboard[4] == self.oxoboard[7] != 0:
+            return self.oxoboard[1]
+        if self.oxoboard[2] == self.oxoboard[5] == self.oxoboard[8] != 0:
+            return self.oxoboard[2]
+        if self.oxoboard[0] == self.oxoboard[4] == self.oxoboard[8] != 0:
+            return self.oxoboard[0]
+        if self.oxoboard[2] == self.oxoboard[4] == self.oxoboard[6] != 0:
+            return self.oxoboard[2]
+        return 0
 
     def show(self):
         """ Display the current board state in the terminal. You should not need to edit this. """
-
+        for y in xrange(3):
+            if y > 0:
+                print "--+---+--"
+            for x in xrange(3):
+                if x > 0:
+                    print '|',
 
                 # Print a space for empty (0), an O for player 1, or an X for player 2
+                print " OX"[self.get_square(x, y)],
+            print
 
 
 def input_square():
